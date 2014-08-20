445eccf1cc64a52352e564069fbdf6a6ffa20e78
Cauê Waneck <waneck@gmail.com>
2014-08-20 02:39:14 -0300

    v 0.1

--- server/src/dispatch/QuestionList.hx
+++ server/src/dispatch/QuestionList.hx
@@ -5,14 +5,15 @@ using Lambda;
 
 class QuestionList
 {
-	public static function run()
+	public static function run(cur:Int=0)
 	{
-		Output.print( new view.QuestionList().setData({ question: getList(1)[0] }).execute() );
+		var list = getList(cur);
+		Output.print( new view.QuestionList().setData({ question: list[0], nextUrl:'/${cur+1}', lastUrl:cur > 0 ?'/${cur-1}' : '#' }).execute() );
 	}
 
 	private static function getList(latest:Int):Array<Question>
 	{
-		return Question.manager.search(1 == 1, { limit: latest, orderBy:[-id] }).array();
+		return Question.manager.search(1 == 1, { limit:[latest,1], orderBy:[-id] }).array();
 	}
 }
 
--- server/src/view/QuestionList.hx
+++ server/src/view/QuestionList.hx
@@ -1,6 +1,6 @@
 package view;
 
 @:includeTemplate('questionlist.html')
-class QuestionList extends erazor.macro.SimpleTemplate<{ question:db.Question }>
+class QuestionList extends erazor.macro.SimpleTemplate<{ question:db.Question, nextUrl:String, lastUrl:String }>
 {
 }
