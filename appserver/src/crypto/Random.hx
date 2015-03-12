995deaf745cc59d2411973fbf260c4e2e64a889b
andiccml <andiccml@gmail.com>
2015-03-12 15:53:04 -0300

    Projeto no FlashDevelop Workaround pro Windows no Random.hx

--- appserver/src/crypto/Random.hx
+++ appserver/src/crypto/Random.hx
@@ -1,16 +1,34 @@
 package crypto;
 
+#if !windows
 import crypto.random.LinuxRandom;
+#else
+import haxe.io.Bytes;
+#end
 
 class Random {
     public static function salt(bytes:Int):String
     {
+		#if !windows
         return LinuxRandom.urandom().read(bytes).toHex();
+		#else
+		var b = Bytes.alloc(bytes);
+		for (i in 0...bytes)
+			b.set(i, Std.random(255));
+		return b.toHex();
+		#end
     }
 
     public static function sid(bytes:Int):String
     {
+		#if !windows
         return LinuxRandom.urandom().read(bytes).toHex();
+		#else
+		var b = Bytes.alloc(bytes);
+		for (i in 0...bytes)
+			b.set(i, Std.random(255));
+		return b.toHex();
+		#end
     }
 }
 
---
+++ appserver/startDB.bat
@@ -0,0 +1,3 @@
+@echo off
+"C:\Program Files\MongoDB\Server\3.0\bin\mongod.exe" --dbpath H:\Work\tikmu\appserver\www\db
+pause
