0efc8f54c74431ef90cf09860ce1ef945f93f146
Jonas Malaco Filho <jonas@jonasmalaco.com>
2015-03-30 10:27:24 -0300

    Began fixing typing errors on { _id : x }, x:String queries
    Requires: jonasmalacofilho/mongo-haxe-driver@18928cd
    Also recommended: jonasmalacofilho/mongo-haxe-managers@431e33f

--- appserver/src/routes/nonroute/NonRouteFunctions.hx
+++ appserver/src/routes/nonroute/NonRouteFunctions.hx
@@ -1,8 +1,10 @@
 package routes.nonroute;
-import db.helper.Ref.Ref;
+
 import db.UserQuestions;
+import db.helper.Ref.Ref;
 import mweb.tools.HttpResponse;
 import mweb.tools.TemplateLink;
+import org.bsonspec.ObjectID;
 import routes.BaseRoute;
 import routes.question.QuestionRoute.QuestionView;
 
@@ -12,13 +14,13 @@ class DeleteQuestion extends BaseRoute
 	public function any(id : String)
 	{
 		var myUser = ctx.session.user.get(ctx.users.col)._id;
-		var q = ctx.questions.findOne( { _id : id } );
+		var q = ctx.questions.findOne( { _id : (id:ObjectID) } );
 		var errorCode = -1; //Question não encontrada
 		if (q != null && q.user == ctx.session.user)
 		{
 			q.deleted = true;
 			q.modified = Date.now();
-			ctx.questions.update( { _id : id }, q);			
+			ctx.questions.update( { _id : (id:ObjectID) }, q);			
 		}
 		return HttpResponse.fromContent(new TemplateLink({ data: { id : q._id, userID : q.user.get(ctx.users.col)._id, userName : q.user.get(ctx.users.col).name, contents : q.contents, tags : q.tags, loc : q.loc, voteSum : q.voteSum, favorites : q.favorites, watchers : q.watchers, date : q.created, solved : q.solved, answers : [for(a in q.answers) { userID : a.user.get(ctx.users.col)._id, deleted : a.deleted, loc : a.loc, voteSum : a.voteSum, date : a.created, contents : a.contents, comments : [for(c in a.comments){userID : c.user.get(ctx.users.col)._id, contents : c.contents, date:c.created, deleted : c.deleted}] } ] }, authenticated : (ctx.session != null), myUser : myUser }, new QuestionView()));
 	}
