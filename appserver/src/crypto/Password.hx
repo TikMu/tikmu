098bd9e532889ecbb48eb630f4402b0703cb12c5
Jonas Malaco Filho <jonas@jonasmalaco.com>
2015-04-19 05:50:11 -0300

    Remove forgetten trace "hashing"

--- appserver/src/crypto/Password.hx
+++ appserver/src/crypto/Password.hx
@@ -31,7 +31,6 @@ abstract Password(String) {
 		case SPlain:
 			plain;
 		case SSha256(it, salt):
-			trace("hashing");
 			var h = plain + salt + plain;
 			for (i in 0...it)
 				h = haxe.crypto.Sha256.encode(h);
