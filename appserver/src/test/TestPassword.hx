48d818c2a4d80d46d032956ab3a529ff54cffcc7
Jonas Malaco Filho <jonas@jonasmalaco.com>
2015-04-19 05:50:32 -0300

    Faster Password.create/match tests

--- appserver/src/test/TestPassword.hx
+++ appserver/src/test/TestPassword.hx
@@ -16,19 +16,21 @@ class TestPassword {
 		Assert.match(~/^sha256/, Password.create("hello!!!"));
 
 		// plain
-		Assert.equals("plain$hello!!!", Password.create("hello!!!", SPlain));
-		Assert.isTrue(Password.create("hello!!!", SPlain).matches("hello!!!"));
-		Assert.isFalse(Password.create("hello!!!", SPlain).matches("hello!!"));
+		var p = Password.create("hello!!!", SPlain);
+		Assert.equals("plain$hello!!!", p);
+		Assert.isTrue(p.matches("hello!!!"));
+		Assert.isFalse(p.matches("hello!!"));
 		Assert.isTrue(( "plain$hello!!!":Password ).matches("hello!!!"));
 
 		// sha256
-		Assert.match(~/^sha256\$\d+\$[a-z0-9]+\$[a-z0-9]+$/, Password.create("hello!!!"));
-		Assert.isTrue(Password.create("hello!!!").matches("hello!!!"));
-		Assert.isFalse(Password.create("hello!!!").matches("hello!!"));
+		var p = Password.create("hello!!!");
+		var q = Password.create("hello!!!");
+		Assert.notEquals(p.hash, q.hash);
+		Assert.match(~/^sha256\$\d+\$[a-z0-9]+\$[a-z0-9]+$/, p);
+		var p = Password.create("hello!!!", SSha256(2, "some salt"));
+		Assert.isTrue(p.matches("hello!!!"));
+		Assert.isFalse(p.matches("hello!!"));
 		Assert.isTrue(Password.fromString("sha256$2$6a6f66$a92e6695aff51167d8148a73d2ba0c8875fb582a13db27bc5059404708de9e62").matches("hello!!!"));
-		var p1 = Password.create("hello!!!");
-		var p2 = Password.create("hello!!!");
-		Assert.notEquals(p1.hash, p2.hash);
 	}
 }
 
