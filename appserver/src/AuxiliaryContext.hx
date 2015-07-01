f01ee7d46266dbd51f4458f27527019fa0402bbb
Jonas Malaco Filho <jonas@jonasmalaco.com>
2015-07-01 19:11:03 -0300

    Views: isolate the html head in ctx.aux.head

--- appserver/src/AuxiliaryContext.hx
+++ appserver/src/AuxiliaryContext.hx
@@ -1,11 +1,26 @@
+typedef HeadViewData = {
+	title : String
+}
+
+@:includeTemplate("route/aux/head.html")
+class HeadView extends BaseView<HeadViewData> {
+	public function new(ctx)
+	{
+		super(ctx);
+		data = { title : null };
+	}
+}
+
 @:includeTemplate("route/aux/menu.html")
 class MenuView extends BaseView<{}> {}
 
 class AuxiliaryContext {
+	public var head(default,null):HeadView;
 	public var menu(default,null):MenuView;
 
 	public function new(ctx)
 	{
+		head = new HeadView(ctx);
 		menu = new MenuView(ctx);
 	}
 }
