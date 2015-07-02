618ae751a6a464b066d94cd7234bd226e16a9fcd
Jonas Malaco Filho <jonas@jonasmalaco.com>
2015-07-02 16:47:56 -0300

    Cache the appserver neko module

--- appserver/src/Main.hx
+++ appserver/src/Main.hx
@@ -2,21 +2,20 @@ import croxit.Web;
 import org.mongodb.*;
 
 class Main {
-	
-	static function main()
-	{
-		haxe.Log.trace = function (msg, ?p) {
-			var s = '[${Date.now()}][${Web.getClientIP()}] ${p.fileName}:${p.lineNumber}:  $msg\n';
-			Sys.stderr().writeString(s);
-		}
+	static var mongo = new Mongo();
+	static var ctx = new Context(mongo.tikmu);
 
-		var mongo = new Mongo();
-		var ctx = new Context(mongo.tikmu);
-
-		/* TODO rinse and repeat */ {
-			ctx.respond();
-		}
+	static function customTrace(msg:String, ?p:haxe.PosInfos) {
+		var s = '[${Date.now()}][${Web.getClientIP()}] ${p.fileName}:${p.lineNumber}:  $msg\n';
+		Sys.stderr().writeString(s);
 	}
 
+	static function main()
+	{
+		haxe.Log.trace = customTrace;
+		neko.Web.cacheModule(main);
+
+		ctx.respond();
+	}
 }
 
