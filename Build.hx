2ca717f8c9f1d3ba76063f0f7e89754e13c5e4d5
Jonas Malaco Filho <jonas@jonasmalaco.com>
2015-07-05 04:22:18 -0300

    Minor: retab Build and Listen
    Despite making this uniform with the rest of the project, this fixes
    some indent issues in a previous commit.

--- Build.hx
+++ Build.hx
@@ -11,167 +11,167 @@ import sys.FileSystem;
 using StringTools;
 
 typedef Define = {
-    name : String,
-    ?value : String
+	name : String,
+	?value : String
 }
 
 enum LibSrc {
-    LOfficial(lib:String);
-    LGit(lib:String, repo:String, ?ref:String, ?src:String);
+	LOfficial(lib:String);
+	LGit(lib:String, repo:String, ?ref:String, ?src:String);
 }
 
 class Git {
-    public static function clone(repo:String, ?path:String, ?branch:String, ?depth:Int)
-    {
-        var args = ["clone", "--recursive"];
-        if (depth != null)
-            args.concat(["--depth", Std.string(depth)]);
-        if (branch != null)
-            args.concat(["--branch", branch]);
-        args.push(repo);
-        if (path != null)
-            args.push(path);
-        return command("git", args);
-    }
+	public static function clone(repo:String, ?path:String, ?branch:String, ?depth:Int)
+	{
+		var args = ["clone", "--recursive"];
+		if (depth != null)
+			args.concat(["--depth", Std.string(depth)]);
+		if (branch != null)
+			args.concat(["--branch", branch]);
+		args.push(repo);
+		if (path != null)
+			args.push(path);
+		return command("git", args);
+	}
 
-    public static function checkout(ref:String)
-        return command("git", ["checkout", ref]);
+	public static function checkout(ref:String)
+		return command("git", ["checkout", ref]);
 }
 
 class Haxelib {
-    public static function install(lib:String)
-        return command("haxelib", ["install", lib]);
+	public static function install(lib:String)
+		return command("haxelib", ["install", lib]);
 
-    public static function dev(lib:String, path:String)
-        return command("haxelib", ["dev", lib, path]);
+	public static function dev(lib:String, path:String)
+		return command("haxelib", ["dev", lib, path]);
 
-    public static function git(lib:String, repo:String)
-        return command("haxelib", ["git", lib, repo]);
+	public static function git(lib:String, repo:String)
+		return command("haxelib", ["git", lib, repo]);
 
-    public static function localSetup()
-        return command("haxelib", ["newrepo"]);
+	public static function localSetup()
+		return command("haxelib", ["newrepo"]);
 
-    public static function linkLocalSetup(path:String)
-        return command("ln", ["--symbolic", '$path/.haxelib', "."]);
+	public static function linkLocalSetup(path:String)
+		return command("ln", ["--symbolic", '$path/.haxelib', "."]);
 
-    public static function smartInstall(src:LibSrc)
-    {
-        switch (src) {
-        case LOfficial(lib):
-            if (install(lib) != 0)
-                throw 'Haxelib error while installing';
-        case LGit(lib, repo, ref, src):
-            // don't use `haxelib git` even when it would suffice to avoid local+dependency issues
-            var cwd = getCwd();
-            var p = './.dev-haxelibs/$lib';
-            if (Git.clone(repo, p) != 0)
-                throw 'Git error while clonning "$repo"';
-            setCwd(p);
-            if (ref != null && Git.checkout(ref) != 0)
-                throw 'Git error while checking out "$ref"';
-            setCwd(cwd);  // can't use local haxelib from a subdirectory
-            var ps = src != null ? p + "/" + src : p;
-            if (dev(lib, ps) != 0)
-                throw 'Haxelib error while setting up a dev path';
-        }
-    }
+	public static function smartInstall(src:LibSrc)
+	{
+		switch (src) {
+		case LOfficial(lib):
+			if (install(lib) != 0)
+				throw 'Haxelib error while installing';
+		case LGit(lib, repo, ref, src):
+			// don't use `haxelib git` even when it would suffice to avoid local+dependency issues
+			var cwd = getCwd();
+			var p = './.dev-haxelibs/$lib';
+			if (Git.clone(repo, p) != 0)
+				throw 'Git error while clonning "$repo"';
+			setCwd(p);
+			if (ref != null && Git.checkout(ref) != 0)
+				throw 'Git error while checking out "$ref"';
+			setCwd(cwd);  // can't use local haxelib from a subdirectory
+			var ps = src != null ? p + "/" + src : p;
+			if (dev(lib, ps) != 0)
+				throw 'Haxelib error while setting up a dev path';
+		}
+	}
 
 }
 
 class Build {
 
-    static var libs = [
-        LOfficial("hscript"),  // for erazor
+	static var libs = [
+		LOfficial("hscript"),  // for erazor
 
-        LOfficial("croxit-1"),
-        LGit("mongodb", "https://github.com/jonasmalacofilho/mongo-haxe-driver.git", "managers"),
-        LGit("mongodb-managers", "https://github.com/jonasmalacofilho/mongo-haxe-managers.git", "master", "lib"),
-        LGit("geotools", "https://github.com/waneck/geotools.git"),
-        LGit("mweb", "https://github.com/waneck/mweb.git", null, "src"),
-        LGit("erazor", "https://github.com/waneck/erazor.git"),
-    ];
+		LOfficial("croxit-1"),
+		LGit("mongodb", "https://github.com/jonasmalacofilho/mongo-haxe-driver.git", "managers"),
+		LGit("mongodb-managers", "https://github.com/jonasmalacofilho/mongo-haxe-managers.git", "master", "lib"),
+		LGit("geotools", "https://github.com/waneck/geotools.git"),
+		LGit("mweb", "https://github.com/waneck/mweb.git", null, "src"),
+		LGit("erazor", "https://github.com/waneck/erazor.git"),
+	];
 
-    static function customTrace(msg:String, ?p:haxe.PosInfos) {
-        if (p.customParams != null)
-            msg = msg + ',' + p.customParams.join(',');
-        var s = '${p.fileName}:${p.lineNumber}:  $msg\n';
-        Sys.stderr().writeString(s);
-    }
+	static function customTrace(msg:String, ?p:haxe.PosInfos) {
+		if (p.customParams != null)
+			msg = msg + ',' + p.customParams.join(',');
+		var s = '${p.fileName}:${p.lineNumber}:  $msg\n';
+		Sys.stderr().writeString(s);
+	}
 
-    static function rmrf(path:String)
-    {
-        // this is fucking dangerous, considering path errors and escaping
-        // issues...
-        //
-        // don't use this script with this unless you're brave and have nothing
-        // to loose!  you have been warned.
-        return command("rm", ["-rf", path]);
-    }
+	static function rmrf(path:String)
+	{
+		// this is fucking dangerous, considering path errors and escaping
+		// issues...
+		//
+		// don't use this script with this unless you're brave and have nothing
+		// to loose!  you have been warned.
+		return command("rm", ["-rf", path]);
+	}
 
-    static function build(haxeArgs:Array<String>)
-    {
-        trace("Fetching haxelibs and other dependencies");
-        if (Haxelib.localSetup() != 0)
-            throw "Haxelib error while setting up locally";
-        for (lib in libs) {
-            switch (lib) {
-            case LOfficial(lib):
-                trace('Installing official "$lib" haxelib');
-            case LGit(lib, repo, ref, _):
-                trace('Installing "$lib" from ' + (ref != null ? '"$repo@$ref"' : '"$repo"'));
-            }
-            Haxelib.smartInstall(lib);
-        }
+	static function build(haxeArgs:Array<String>)
+	{
+		trace("Fetching haxelibs and other dependencies");
+		if (Haxelib.localSetup() != 0)
+			throw "Haxelib error while setting up locally";
+		for (lib in libs) {
+			switch (lib) {
+			case LOfficial(lib):
+				trace('Installing official "$lib" haxelib');
+			case LGit(lib, repo, ref, _):
+				trace('Installing "$lib" from ' + (ref != null ? '"$repo@$ref"' : '"$repo"'));
+			}
+			Haxelib.smartInstall(lib);
+		}
 
-        trace("Building the appserver");
-        trace('Moving into "./appserver"');
-        setCwd("appserver");
-        if (Haxelib.linkLocalSetup("..") != 0)  // can't use local haxelib from a subdirectory
-            throw "Error while linking to another local haxelib setup";
-        trace("Running haxe");
-        var args = haxeArgs.concat(["build.hxml"]);
-        if (command("haxe", args) != 0)
-            throw "Failed compilation";
-    }
+		trace("Building the appserver");
+		trace('Moving into "./appserver"');
+		setCwd("appserver");
+		if (Haxelib.linkLocalSetup("..") != 0)	// can't use local haxelib from a subdirectory
+			throw "Error while linking to another local haxelib setup";
+		trace("Running haxe");
+		var args = haxeArgs.concat(["build.hxml"]);
+		if (command("haxe", args) != 0)
+			throw "Failed compilation";
+	}
 
-    public static function begin(baseDir:String, commit:String, buildDir:String, haxeArgs:Array<String>)
-    {
-        baseDir = FileSystem.absolutePath(baseDir);
-        buildDir = FileSystem.absolutePath(buildDir);
+	public static function begin(baseDir:String, commit:String, buildDir:String, haxeArgs:Array<String>)
+	{
+		baseDir = FileSystem.absolutePath(baseDir);
+		buildDir = FileSystem.absolutePath(buildDir);
 
-        setCwd(baseDir);  // make sure we start at someplace safe, otherwise git might fail
+		setCwd(baseDir);  // make sure we start at someplace safe, otherwise git might fail
 
-        trace('Cloning "$baseDir" into build dir "$buildDir"');
-        if (FileSystem.exists(buildDir) && FileSystem.isDirectory(buildDir)) {
-            trace("Deleting previous build directory");
-            if (rmrf(buildDir) != 0)
-                throw "Possibly dangerous rm error";
-        }
-        if (Git.clone(baseDir, buildDir) != 0)
-            throw 'Git error while cloning';
-        trace('Changing current working dir to "$buildDir"');
-        setCwd(buildDir);
-        trace('Checking out commit "$commit"');
-        if (Git.checkout(commit) != 0)
-            throw 'Git error while checking out';
+		trace('Cloning "$baseDir" into build dir "$buildDir"');
+		if (FileSystem.exists(buildDir) && FileSystem.isDirectory(buildDir)) {
+			trace("Deleting previous build directory");
+			if (rmrf(buildDir) != 0)
+				throw "Possibly dangerous rm error";
+		}
+		if (Git.clone(baseDir, buildDir) != 0)
+			throw 'Git error while cloning';
+		trace('Changing current working dir to "$buildDir"');
+		setCwd(buildDir);
+		trace('Checking out commit "$commit"');
+		if (Git.checkout(commit) != 0)
+			throw 'Git error while checking out';
 
-        trace("Running the checkout build recipe");
-        if (command("haxe", ["--run", "Build"].concat(haxeArgs)) != 0)
-            throw "Failed compilation";
+		trace("Running the checkout build recipe");
+		if (command("haxe", ["--run", "Build"].concat(haxeArgs)) != 0)
+			throw "Failed compilation";
 
-        trace('Moving back to "$baseDir"');
-        setCwd(baseDir);
-    }
+		trace('Moving back to "$baseDir"');
+		setCwd(baseDir);
+	}
 
-    static function main()
-    {
-        try {
-            build(Sys.args());
-        } catch (e:Dynamic) {
-            trace('ERROR: $e');
-            exit(-1);
-        }
-    }
+	static function main()
+	{
+		try {
+			build(Sys.args());
+		} catch (e:Dynamic) {
+			trace('ERROR: $e');
+			exit(-1);
+		}
+	}
 
 }
 
--- documentation/server-setup/github-listener/Listen.hx
+++ documentation/server-setup/github-listener/Listen.hx
@@ -8,199 +8,199 @@ typedef Path = String;
 typedef Url = String;
 
 typedef ShortUser = {
-    name : String,  // this actually is the username
-    email : String
+	name : String,	// this actually is the username
+	email : String
 }
 
 typedef User = {
-    name : String,
-    email : String,
-    username : String
+	name : String,
+	email : String,
+	username : String
 }
 
 typedef Commit = {
-    id : String,
-    distinct : Bool,
-    message : String,
-    timestamp : String,
-    url : Url,
-    author : User,
-    committer : User,
-    added : Array<Path>,
-    removed : Array<Path>,
-    modified : Array<Path>
+	id : String,
+	distinct : Bool,
+	message : String,
+	timestamp : String,
+	url : Url,
+	author : User,
+	committer : User,
+	added : Array<Path>,
+	removed : Array<Path>,
+	modified : Array<Path>
 }
 
 typedef Sender = Dynamic;
 
 typedef Repository = {
-    id : Int,
-    name : String,
-    full_name : String,
-    owner : ShortUser,
-    html_url : Url,
-    description : String,
-    fork : Bool,
-    url : Url,
-    created_at : Dynamic,
-    update_at : Dynamic,
-    pushed_at : Dynamic,
-    default_branch : String,
-    master_branch : String
-    // more properties
-    // more urls
+	id : Int,
+	name : String,
+	full_name : String,
+	owner : ShortUser,
+	html_url : Url,
+	description : String,
+	fork : Bool,
+	url : Url,
+	created_at : Dynamic,
+	update_at : Dynamic,
+	pushed_at : Dynamic,
+	default_branch : String,
+	master_branch : String
+		// more properties
+		// more urls
 }
 
 typedef PushEvent = {
-    ref : String,
-    before : String,
-    after : String,
-    created : Bool,
-    deleted : Bool,
-    forced : Bool,
-    base_ref : Dynamic,
-    compare : Url,
-    commits : Array<Commit>,
-    head_commit : Commit,
-    repository : Repository,
-    pusher : ShortUser,
-    sender : Sender
+	ref : String,
+	before : String,
+	after : String,
+	created : Bool,
+	deleted : Bool,
+	forced : Bool,
+	base_ref : Dynamic,
+	compare : Url,
+	commits : Array<Commit>,
+	head_commit : Commit,
+	repository : Repository,
+	pusher : ShortUser,
+	sender : Sender
 }
 
 class Listen {
-    static var config = {
-        secret : "not much of a secret, but will have to make due for now",
-        remote : "origin",
-        repository : "jonasmalacofilho/tikmu",
-        baseDir : "/var/build/tikmu",
-        baseBuildDir : "/var/build/tikmu-builds",
-        baseOutputDir : "/var/www/tikmu",
-        haxeArgs : ["-D", "tikmu_require_login", "-D", "tikmu_cache_module"]
-    }
+	static var config = {
+		secret : "not much of a secret, but will have to make due for now",
+		remote : "origin",
+		repository : "jonasmalacofilho/tikmu",
+		baseDir : "/var/build/tikmu",
+		baseBuildDir : "/var/build/tikmu-builds",
+		baseOutputDir : "/var/www/tikmu",
+		haxeArgs : ["-D", "tikmu_require_login", "-D", "tikmu_cache_module"]
+	}
 
-    static function customTrace(msg:String, ?p:haxe.PosInfos) {
-        if (p.customParams != null)
-            msg = msg + ',' + p.customParams.join(',');
-        var s = '[${Web.getClientIP()}] ${p.fileName}:${p.lineNumber}:  $msg\n';
-        Sys.stderr().writeString(s);
-    }
+	static function customTrace(msg:String, ?p:haxe.PosInfos) {
+		if (p.customParams != null)
+			msg = msg + ',' + p.customParams.join(',');
+		var s = '[${Web.getClientIP()}] ${p.fileName}:${p.lineNumber}:	$msg\n';
+		Sys.stderr().writeString(s);
+	}
 
-    static function verifiedSig(data:String, sig:String)
-    {
-        var sig = sig.toLowerCase().split("=");
-        if (sig[0] != "sha1")
-            throw 'Unsupported hash algorithm in signature: ${sig[0]}';
-        var secret = Bytes.ofString(config.secret);
-        var data = Bytes.ofString(data);
-        var hmac = new Hmac(SHA1).make(secret, data);
-        return hmac.toHex() == sig[1];
-    }
+	static function verifiedSig(data:String, sig:String)
+	{
+		var sig = sig.toLowerCase().split("=");
+		if (sig[0] != "sha1")
+			throw 'Unsupported hash algorithm in signature: ${sig[0]}';
+		var secret = Bytes.ofString(config.secret);
+		var data = Bytes.ofString(data);
+		var hmac = new Hmac(SHA1).make(secret, data);
+		return hmac.toHex() == sig[1];
+	}
 
-    static function rmrf(path:String)
-    {
-        // this is fucking dangerous, considering path errors and escaping
-        // issues...
-        //
-        // don't use this script with this unless you're brave and have nothing
-        // to loose!  you have been warned.
-        if (command("rm", ["-rf", path]) != 0)
-            throw 'Failed rm -rf command (path: $path)';
-    }
+	static function rmrf(path:String)
+	{
+		// this is fucking dangerous, considering path errors and escaping
+		// issues...
+		//
+		// don't use this script with this unless you're brave and have nothing
+		// to loose!  you have been warned.
+		if (command("rm", ["-rf", path]) != 0)
+			throw 'Failed rm -rf command (path: $path)';
+	}
 
-    static function cpr(origin:String, destination:String)
-    {
-        var args = ["-r", origin, destination];
-        if (command("cp", args) != 0)
-            throw 'Failed copy command (origin: $origin, destination: $destination)';
-    }
+	static function cpr(origin:String, destination:String)
+	{
+		var args = ["-r", origin, destination];
+		if (command("cp", args) != 0)
+			throw 'Failed copy command (origin: $origin, destination: $destination)';
+	}
 
-    static function respond()
-    {
-        var data = Web.getPostData();
-        var sig = Web.getClientHeader("X-Hub-Signature");
-        if (!verifiedSig(data, sig)) {
-            Web.setReturnCode(403);  // forbidden
-            return;
-        }
+	static function respond()
+	{
+		var data = Web.getPostData();
+		var sig = Web.getClientHeader("X-Hub-Signature");
+		if (!verifiedSig(data, sig)) {
+			Web.setReturnCode(403);  // forbidden
+			return;
+		}
 
-        var delivery = Web.getClientHeader("X-GitHub-Delivery");
-        trace('Delivery: $delivery');
+		var delivery = Web.getClientHeader("X-GitHub-Delivery");
+		trace('Delivery: $delivery');
 
-        var event = Web.getClientHeader("X-GitHub-Event").toLowerCase();
-        trace('Event: $event');
-        if (event == "ping") {
-            Web.setReturnCode(200);
-            return;
-        }
-        if (event != "push") {
-            Web.setReturnCode(417);  // expectation failed
-                trace('ERROR: Expecting "push" or "ping" events');
-                return;
-            }
+		var event = Web.getClientHeader("X-GitHub-Event").toLowerCase();
+		trace('Event: $event');
+		if (event == "ping") {
+			Web.setReturnCode(200);
+			return;
+		}
+		if (event != "push") {
+			Web.setReturnCode(417);  // expectation failed
+			trace('ERROR: Expecting "push" or "ping" events');
+			return;
+		}
 
-            var push = (haxe.Json.parse(data):PushEvent);
+		var push = (haxe.Json.parse(data):PushEvent);
 
-            if (push.repository.full_name != config.repository) {
-                Web.setReturnCode(417);  // expectation failed
-                trace('ERROR: Expecting repository to be "${config.repository}"');
-                return;
-            }
+		if (push.repository.full_name != config.repository) {
+			Web.setReturnCode(417);  // expectation failed
+			trace('ERROR: Expecting repository to be "${config.repository}"');
+			return;
+		}
 
-            var branch = push.ref.replace("refs/heads/", "");
-            var buildDir = '${config.baseBuildDir}/$branch';
-            var outputDir = '${config.baseOutputDir}/$branch';
+		var branch = push.ref.replace("refs/heads/", "");
+		var buildDir = '${config.baseBuildDir}/$branch';
+		var outputDir = '${config.baseOutputDir}/$branch';
 
-            if (push.deleted) {
-                Web.setReturnCode(202);  // accepted
-                trace('Accepted remove request for branch "$branch"');
-                rmrf(outputDir);
-                return;
-            }
+		if (push.deleted) {
+			Web.setReturnCode(202);  // accepted
+			trace('Accepted remove request for branch "$branch"');
+			rmrf(outputDir);
+			return;
+		}
 
-            var head = push.head_commit.id;
-            trace('Accepted build request for branch "$branch" (head is "${head.substr(0,7)}")');
+		var head = push.head_commit.id;
+		trace('Accepted build request for branch "$branch" (head is "${head.substr(0,7)}")');
 
-            trace('Fetching from ${config.remote}');
-            setCwd(config.baseDir);
-            command("git", ["fetch", config.remote]);
+		trace('Fetching from ${config.remote}');
+		setCwd(config.baseDir);
+		command("git", ["fetch", config.remote]);
 
-            trace('Calling a build to "$buildDir" with ${config.haxeArgs}');
-            Build.begin(config.baseDir, head, buildDir, config.haxeArgs);
+		trace('Calling a build to "$buildDir" with ${config.haxeArgs}');
+		Build.begin(config.baseDir, head, buildDir, config.haxeArgs);
 
-            trace('Installing to "$outputDir"');
-            rmrf(outputDir);
-            cpr('$buildDir/appserver/www', outputDir);
+		trace('Installing to "$outputDir"');
+		rmrf(outputDir);
+		cpr('$buildDir/appserver/www', outputDir);
 
-            trace("Adding infos.json");
-            var infos = {
-                built_at : Date.now(),
-                branch : branch,
-                commit : {
-                    id : push.head_commit.id,
-                    timestamp : push.head_commit.timestamp,
-                    author : push.head_commit.author.name,
-                    message : push.head_commit.message,
-                }
-            }
-            sys.io.File.saveContent('$outputDir/infos.json', haxe.Json.stringify(infos));
+		trace("Adding infos.json");
+		var infos = {
+			built_at : Date.now(),
+			branch : branch,
+			commit : {
+				id : push.head_commit.id,
+				timestamp : push.head_commit.timestamp,
+				author : push.head_commit.author.name,
+				message : push.head_commit.message,
+			}
+		}
+		sys.io.File.saveContent('$outputDir/infos.json', haxe.Json.stringify(infos));
 
-            Web.setReturnCode(200);  // OK
-            println("Build successfull");
-        }
+		Web.setReturnCode(200);  // OK
+		println("Build successfull");
+	}
 
-        static function main()
-        {
-            haxe.Log.trace = customTrace;
-            Web.cacheModule(main);
+	static function main()
+	{
+		haxe.Log.trace = customTrace;
+		Web.cacheModule(main);
 
-            try {
-                respond();
-            } catch (e:Dynamic) {
-                Web.setReturnCode(500);  // Internal Server Error
-                println('Build aborted with error: $e');
-                var s = haxe.CallStack.exceptionStack();
-                trace("Call stack: " + haxe.CallStack.toString(s));
-        }
-    }
+		try {
+			respond();
+		} catch (e:Dynamic) {
+			Web.setReturnCode(500);  // Internal Server Error
+			println('Build aborted with error: $e');
+			var s = haxe.CallStack.exceptionStack();
+			trace("Call stack: " + haxe.CallStack.toString(s));
+		}
+	}
 }
 
