52100a608775d60970ca1f9e9ab443b5ce887ae9
Jonas Malaco Filho <jonas@jonasmalaco.com>
2015-07-25 19:25:28 -0300

    UI: immediately and optimistically update the fav/follow icons
    Requires some cleanup.  Move all TikMu behaviour code to pkg `tikmu`
    (and move `reputation.Handler` to `tikmu.Reputation`), including the
    special favorite/following relationship.
    
    Also, the UI requires some "saving..." status, with optional "failed" on
    errors.  Currently, the previous state of the icons is restore
    immediately, and this will certainly cause confusion.

--- appserver/src/ui/QuestionActions.hx
+++ appserver/src/ui/QuestionActions.hx
@@ -13,16 +13,35 @@ class QuestionActions {
 	{
 		var elem = J(e.target);
 		var qid = elem.parents("article.question").attr("id");
-		function setIcons(state:{ favorite:Bool, following:Bool }) {
-			var icons = elem.siblings().add(elem);
-			var favorite = icons.filter(".favorite");
-			var follow = icons.filter(".follow");
+		var icons = elem.siblings().add(elem);
+		var favorite = icons.filter(".favorite");
+		var follow = icons.filter(".follow");
+
+		function getState() {
+			return {
+				favorite : favorite.hasClass("pressed"),
+				following : follow.hasClass("pressed")
+			}
+		}
+		function applyState(state:{ favorite:Bool, following:Bool }) {
 			if (state.favorite != favorite.hasClass("pressed"))
 				favorite.toggleClass("pressed");
 			if (state.following != follow.hasClass("pressed"))
 				follow.toggleClass("pressed");
 		}
-		JQuery.post('/question/$qid/$action', setIcons, "text json");
+
+		var curState = getState();
+		if (elem.is(favorite))
+			applyState({
+				favorite : !curState.favorite,
+				following : curState.following && !curState.favorite
+			});
+		else
+			applyState({
+				favorite : curState.favorite || !curState.following,
+				following : !curState.following
+			});
+		JQuery.post('/question/$qid/$action', applyState, "text json").fail(applyState.bind(curState));
 		e.preventDefault();
 	}
 
