e4f8c5ec836a10a5e570045d7c42ddba5c55c8ef
Jonas Malaco Filho <jonas@jonasmalaco.com>
2015-07-26 21:58:30 -0300

    UI: optimistically update the up/down vote icons and vote count
    Note: the vote count has always been an estimation, since the actual
    value is calculated by the reputation system (although it's quite
    predictable).
    
    Closes #18.

--- appserver/src/ui/AnswerActions.hx
+++ appserver/src/ui/AnswerActions.hx
@@ -12,28 +12,33 @@ class AnswerActions {
 	static function answerAction(action:AnswerAction, e:Event)
 	{
 		var elem = J(e.target);
+		var aid = elem.parents("article.answer").attr("id");
+		var spans = elem.siblings().add(elem);
+		var upvote = spans.filter(".upvote");
+		var downvote = spans.filter(".downvote");
+		var count = spans.filter(".vote_count");
+
 		if (elem.hasClass("pressed"))
 			action = action != QDoUpvote ? QDoUpvote : QDoDownvote;
 
-		var aid = elem.parents("article.answer").attr("id");
+		var inc = action == QDoUpvote ? 1 : -1;
+
 		function style(state : { vote : Int }) {
-			var spans = elem.siblings().add(elem);
-			var upvote = spans.filter(".upvote");
-			var downvote = spans.filter(".downvote");
 			switch (state.vote) {
 			case  1: upvote.addClass("pressed"); downvote.removeClass("pressed");
 			case  0: upvote.removeClass("pressed"); downvote.removeClass("pressed");
 			case -1: upvote.removeClass("pressed"); downvote.addClass("pressed");
 			}
 
-			var count = spans.filter(".vote_count");
-			var val = Std.parseInt(count.html());
-			switch (action) {
-			case QDoUpvote: count.html(Std.string(val + 1));
-			case QDoDownvote: count.html(Std.string(val - 1));
-			}
+			count.html(Std.string(Std.parseInt(count.html()) + inc));
 		}
-		JQuery.post('/answer/$aid/$action', style, "text json");
+		function onFail() {
+			elem.toggleClass("pressed");
+			count.html(Std.string(Std.parseInt(count.html()) - inc));
+		}
+
+		elem.toggleClass("pressed");
+		JQuery.post('/answer/$aid/$action', style, "text json").fail(onFail);
 		e.preventDefault();
 	}
 
