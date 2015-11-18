882c245da249e68a09aa59338f79ce67b57498a8
Flávio Fraschetti <frasqueti@gmail.com>
2015-11-18 19:42:03 -0200

    replace html main tag

--- appserver/www/js/modal.js
+++ appserver/www/js/modal.js
@@ -1,10 +1,11 @@
 $(document).ready(function(){
-	$(".question_card_question > a, .question_card_answers > a").click(function(){
-    	console.log(this.href);
-    	$.get(this.href).done(function(x){
-    		console.log(x);
+	$(".question_card_question > a, .question_card_answers > a").click(function(clk){
+    	$.get(this.href).done(function(html){
+    		var data = $(html).$("main.question_open");
+    		var question = $("<div class='question_open modal'></div>");
+    		data.appendTo(question);
+    		$("main").after(question);
     	});
+    	clk.preventDefault();
   	});
 });
-
-
