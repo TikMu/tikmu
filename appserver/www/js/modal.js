4cea9961726631fe8c35feca8df2440eef36ca10
Flávio Fraschetti <frasqueti@gmail.com>
2015-11-18 20:21:49 -0200

    fix

--- appserver/www/js/modal.js
+++ appserver/www/js/modal.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 	$(".question_card_question > a, .question_card_answers > a").click(function(clk){
     	$.get(this.href).done(function(html){
-    		var data = $(html).find("main.question_open");
+    		var data = $(html).filter("main.question_open");
     		var question = $("<div class='question_open modal'></div>");
     		data.appendTo(question);
     		$("main").after(question);
