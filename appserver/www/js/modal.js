9a04c71b544e272358cb9884ffae727094f3d64b
Flávio Fraschetti <frasqueti@gmail.com>
2015-11-23 16:17:05 -0200

    fix

--- appserver/www/js/modal.js
+++ appserver/www/js/modal.js
@@ -1,17 +1,15 @@
 $(document).ready(function(){
 	$(".question_card_question > a, .question_card_answers > a").click(function(clk){
     	$.get(this.href).done(function(html){
-    		var data = $(html).filter("main.question_open");
+    		var data = $(html).filter("div.question_open");
     		var question = $("<div class='question_open modal'></div>");
     		data.appendTo(question);
-    		$("main").after(question);
+    		$("div").after(question);
                 $(".overlay").click(function() {
-                $(".question_open.modal, question_open").hide('fast', function() {
+                $(".question_open.modal, div.question_open").hide('fast', function() {
             });
         });
     	});
     	clk.preventDefault();
   	});
 });
-
-
