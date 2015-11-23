f5d3d87cef50eb8b3922c78ab11b7b36f8f74852
Flávio Fraschetti <frasqueti@gmail.com>
2015-11-23 16:21:18 -0200

    fix

--- appserver/www/js/modal.js
+++ appserver/www/js/modal.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
     		var data = $(html).filter("div.question_open");
     		var question = $("<div class='question_open modal'></div>");
     		data.appendTo(question);
-    		$("div").after(question);
+    		$("div.question_open").after(question);
                 $(".overlay").click(function() {
                 $(".question_open.modal, div.question_open").hide('fast', function() {
             });
