6c7b9eec7646eb4fd83608bf05d0d9bcd582018f
Flávio Fraschetti <frasqueti@gmail.com>
2015-11-25 19:34:04 -0200

    modal fade out effect

--- appserver/www/js/modal.js
+++ appserver/www/js/modal.js
@@ -1,15 +1,14 @@
 $(document).ready(function(){
-	$(".question_card_question > a, .question_card_answers > a").click(function(clk){
-    	$.get(this.href).done(function(html){
-    		var data = $(html).filter("div.question_open");
-    		var question = $("<div class='question_open modal'></div>");
-    		data.appendTo(question);
-    		$("main").after(question);
+    $(".question_card_question > a, .question_card_answers > a").click(function(clk){
+        $.get(this.href).done(function(html){
+            var data = $(html).filter("div.question_open");
+            var question = $("<div class='question_open modal'></div>");
+            data.appendTo(question);
+            $("main").after(question);
                 $(".overlay, .close").click(function() {
-                $(".question_open.modal, div.question_open").hide('fast', function() {
-            });
+                $(".question_open.modal, div.question_open").fadeOut('slow');
         });
-    	});
-    	clk.preventDefault();
-  	});
+        });
+        clk.preventDefault();
+    });
 });
