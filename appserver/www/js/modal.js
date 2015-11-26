435d52a5ac78eeedc2d06186b9dcfc9f0fec38c4
Flávio Fraschetti <frasqueti@gmail.com>
2015-11-26 18:15:05 -0200

    Fix

--- appserver/www/js/modal.js
+++ appserver/www/js/modal.js
@@ -4,16 +4,17 @@ $(document).ready(function(){
             var data = $(html).filter("div.question_open");
             var question = $("<div class='question_open modal'></div>");
             data.appendTo(question);
+            $("div.question_open.modal, .div.question_open, .overlay").fadeIn('slow');
+            $("div.overlay").css("opacity", "60");
+             
             $("main").after(question);
-            $("article.question.question_card").addClass ("blurred");
+            $("article.question.question_card").addClass("blurred");
+
         $(".overlay, .close").click(function() {
             $("div.question_open.modal, .div.question_open").fadeOut('slow');
-            $("article.question.question_card, .div.question_open.modal").removeClass ("blurred overlay");    
+            $("article.question.question_card, .div.question_open.modal").removeClass ("blurred");    
         });
         });
         clk.preventDefault();
     });
-});
-$(".question_card_question > a, .question_card_answers > a").click(function(clk){
-$(".question_open.modal, div.question_open").fadeIn('slow');
 });
