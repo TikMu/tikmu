42238a493104b12e59b80e2a661f67a1755c6961
Flávio Fraschetti <frasqueti@gmail.com>
2015-11-26 16:51:05 -0200

    Testing FadeIn when open card

--- appserver/www/js/modal.js
+++ appserver/www/js/modal.js
@@ -6,8 +6,10 @@ $(document).ready(function(){
             data.appendTo(question);
             $("main").after(question);
             $("article.question.question_card").addClass ("blurred");
-                $(".overlay, .close").click(function() {
-                $(".question_open.modal, div.question_open").fadeOut('slow');
+            $(".question_open.modal, div.question_open").fadeIn('slow');
+        
+        $(".overlay, .close").click(function() {
+            $(".question_open.modal, div.question_open").fadeOut('slow');
             $("article.question.question_card").removeClass ("blurred");    
         });
         });
