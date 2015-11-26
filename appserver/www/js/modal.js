5f7e08c57f7c2492290eb7704cbea0fe0603ccac
Flávio Fraschetti <frasqueti@gmail.com>
2015-11-26 17:48:09 -0200

    Fix

--- appserver/www/js/modal.js
+++ appserver/www/js/modal.js
@@ -6,8 +6,6 @@ $(document).ready(function(){
             data.appendTo(question);
             $("main").after(question);
             $("article.question.question_card").addClass ("blurred");
-            $("div.question_open.modal, .div.question_open").fadeIn('slow');
-        
         $(".overlay, .close").click(function() {
             $("div.question_open.modal, .div.question_open").fadeOut('slow');
             $("article.question.question_card, .div.question_open.modal").removeClass ("blurred overlay");    
@@ -15,4 +13,7 @@ $(document).ready(function(){
         });
         clk.preventDefault();
     });
+});
+$(".question_card_question > a, .question_card_answers > a").click(function(clk){
+$(".question_open.modal, div.question_open").fadeIn('slow');
 });
