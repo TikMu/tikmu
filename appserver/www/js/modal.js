13cd91e646467c26b3fea9f771031bbda70db458
Flávio Fraschetti <frasqueti@gmail.com>
2015-11-30 14:37:16 -0200

    JS modal

--- appserver/www/js/modal.js
+++ appserver/www/js/modal.js
@@ -4,12 +4,31 @@ $(document).ready(function(){
             var data = $(html).filter("div.question_open");
             var question = $("<div class='question_open modal'></div>");
             data.appendTo(question);
-            $("article.question.question_open").fadeTo("slow", 1);
+            
+
+            $("article.question.question_open").fadeTo("slow", 1); 
             $("main").after(question);
+
+            var windowOffset = $(document).offset().top;
+            var questionHeight = question.height();
+            var windowHeight = $(window).height();
+            var positionModal;
+
+            // positioning the modal relative to scrolled window
+
+            if (questionHeight <= windowHeight - 60) {
+                positionModal = windowOffset + ((windowHeight - 60 - questionHeight) / 2);
+            }
+            else {
+                positionModal = windowOffset + 30;
+            }
+
+            $('.question_open_inner').css({'top':positionModal});
+
             $("article.question.question_card").addClass("blurred");
 
         $(".overlay, .close").click(function() {
-            $("div.question_open.modal, .div.question_open").fadeTo("slow", 0);;
+            $("div.question_open.modal, .div.question_open").fadeTo("slow", 0);; 
             $("article.question.question_card").removeClass ("blurred");
             $("div.question_open.modal, .div.question_open, .overlay").detach();   
         });
@@ -17,3 +36,4 @@ $(document).ready(function(){
         clk.preventDefault();
     });
 });
+
