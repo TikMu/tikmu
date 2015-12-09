d5a43123f3489923615304cc3ef664b28ea6b674
Flávio Fraschetti <frasqueti@gmail.com>
2015-12-09 07:40:57 -0200

    rewriting code for best practices

--- appserver/www/js/modal.js
+++ appserver/www/js/modal.js
@@ -6,31 +6,14 @@ $(document).ready(function(){
             data.appendTo(question);
             $("main").after(question);
 
-            // var windowOffset = $(document).scrollTop();
-            // var questionHeight = $('.question_open_inner').height();
-            // var windowHeight = $(window).height();
-            // var positionModal;
-
-            // positioning the modal relative to scrolled window
-
-            // if (questionHeight <= windowHeight - 60) {
-            //     positionModal = windowOffset + ((windowHeight - 60 - questionHeight) / 2);
-            // }
-            // else {
-            //     positionModal = windowOffset + 120;
-            // }
-            $('div.question_open.modal, .div.question_open, .overlay').fadeIn(800); 
-            $('.question_open_inner').css({'top':200});
-            $('body').css({'overflow':'hidden'});
-            $('div.question_open.modal').css({'display':'block'});
-            $('article.question.question_card').addClass('blurred');
+            $('div.question_open.modal, .div.question_open, .overlay').fadeIn('slow'); 
+            $('article.question.question_card').toggleClass('is-blurred');
 
         $(".overlay, .close").click(function() {
-            $('div.question_open.modal, .div.question_open, .overlay').fadeOut(600, function() { 
-        $(this).remove(); 
-
-            $("article.question.question_card").removeClass ('blurred');
-            $('body').css({'overflow':'auto'});
+            $('div.question_open.modal, .div.question_open, .overlay').fadeOut('slow', function() { 
+                $(this).remove(); 
+                $('article.question.question_card').toggleClass('is-blurred');
+                $('body').toggleClass('is-locked');
             });
             });
         });
