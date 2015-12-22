4e429dcb77d243dfdf13dcf57755f1c46657b264
Jonas Malaco Filho <jonas@jonasmalaco.com>
2015-12-22 07:33:29 -0200

    Auth: try to make cookies universal between old and new build systems

--- appserver/src/Context.hx
+++ appserver/src/Context.hx
@@ -35,7 +35,7 @@ class Context {
 				trace('${h.header}: ${h.value}');
 		}
 
-		var hpat = ~/^(.+?\.)?(.+)$/;
+		var hpat = ~/^(.+?\.)?(new\.)?(.+)$/;
 		if (hpat.match(Web.getHostName())) {
 			domain = hpat.matched(2);
 			subdomain = hpat.matched(1);
